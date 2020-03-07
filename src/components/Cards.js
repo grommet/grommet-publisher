7749c1eb681907e380bf858b1f970b07d77982a2
Eric Soderberg <eric.soderberg@hpe.com>
2020-03-07 05:19:39 -0800

    Tweaked Cards styling

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -5,6 +5,12 @@ import RoutedButton from './RoutedButton';
 
 const colorNameExp = new RegExp('graph');
 
+const textSizeMap = {
+  small: 'medium',
+  medium: 'large',
+  large: 'xlarge',
+};
+
 const Cards = ({ overlay, routes = [], size, ...rest }) => {
   const theme = React.useContext(ThemeContext);
   const colors = Object.keys(theme.global.colors).filter(c =>
@@ -16,20 +22,22 @@ const Cards = ({ overlay, routes = [], size, ...rest }) => {
       margin={overlay ? 'xlarge' : { vertical: 'large' }}
       {...rest}
     >
-      <Grid columns="small" gap="large">
+      <Grid columns="small" gap="large" align="stretch">
         {routes.map((route, index) => (
           <RoutedButton key={route.path} path={route.path} plain>
             {({ hover }) => (
               <Box
-                pad="medium"
+                pad={size}
                 background={{
                   color: colors[index % colors.length],
                   opacity: hover ? 'strong' : undefined,
                 }}
+                fill
                 round
                 align="center"
+                justify="center"
               >
-                <Text size="large" weight="bold">
+                <Text size={textSizeMap[size]} weight="bold" textAlign="center">
                   {route.name}
                 </Text>
               </Box>
